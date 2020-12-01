b7731feffba9d6ec43c4b38b505f96a0dc1272cc
BorislavN <borko_nik@abv.bg>
2020-12-01 14:09:52 +0200

    Added controllers folder

---
+++ api/controller/index.js
@@ -0,0 +1,11 @@
+const userController = require("./userController");
+const carController = require("./carController");
+const offerController = require("./offerController");
+const commentController = require("./commentController");
+
+module.exports = {
+    userController,
+    carController,
+    offerController,
+    commentController
+};
