6cf474b54a78a08f30e19c1ce22fc55a14af8ee8
BorislavN <borko_nik@abv.bg>
2020-12-02 19:11:08 +0200

    Started implementing offerController

---
+++ api/controller/offerController.js
@@ -0,0 +1,35 @@
+const { offerModel } = require("../model");
+
+
+function getAllOffers(req, res, next) {
+
+};
+
+function getMyOffers(req, res, next) {
+
+};
+
+function getOffer(req, res, next) {
+
+};
+
+function addOffer(req, res, next) {
+
+};
+
+function editOffer(req, res, next) {
+
+};
+
+function deleteOffer(req, res, next) {
+
+};
+
+module.exports = {
+    getAllOffers,
+    getMyOffers,
+    getOffer,
+    addOffer,
+    editOffer,
+    deleteOffer
+};
