af93f2e807b2244f37584a58b550ca4fc0e35982
BorislavN <borko_nik@abv.bg>
2020-12-08 00:11:55 +0200

    Fixed a Date format problem

--- api/util/blacklistCleaner.js
+++ api/util/blacklistCleaner.js
@@ -7,7 +7,7 @@ function blacklistCleaner() {
 
     blacklistTokenModel.deleteMany()
         .where("createdAt")
-        .lt(tempDate)
+        .lt(tempDate.getTime())
         .then(result => {
             if (result.ok!==1) {
                 console.log("An error occurred while cleaning the Blacklist!");
