c0951cc78c6690a2370d93c12c1232c1bb76c544
BorislavN <borko_nik@abv.bg>
2020-12-10 17:39:25 +0200

    Fixed a typo in parseUrl method

--- client/src/app/shared/util/url.parser.ts
+++ client/src/app/shared/util/url.parser.ts
@@ -1,6 +1,6 @@
 const cloudUrl = "https://res.cloudinary.com/dqll1uvhe/image/upload/";
 
-export function parseUrl(url: { url: String }, quality: String = " t_default"): String {
+export function parseUrl(url: { url: String }, quality: String = "t_default"): String {
     const result = (url.url.split(cloudUrl)[1]).split("/")[1];
    
     return `${cloudUrl}${quality}/${result}`;
