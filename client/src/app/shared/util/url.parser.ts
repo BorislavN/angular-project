3b4f17fbc546ddcb8ce742f3dd11c5c34e19778d
BorislavN <borko_nik@abv.bg>
2020-12-10 17:23:46 +0200

    Made changes to parseUrl function

--- client/src/app/shared/util/url.parser.ts
+++ client/src/app/shared/util/url.parser.ts
@@ -1,7 +1,10 @@
 const cloudUrl = "https://res.cloudinary.com/dqll1uvhe/image/upload/";
 
-export function parseUrl(url: { url: String }, quality: String = "q_40"): String {
+export function parseUrl(url: { url: String }, quality: String = " t_default"): String {
     const result = (url.url.split(cloudUrl)[1]).split("/")[1];
-
+   
     return `${cloudUrl}${quality}/${result}`;
 };
+
+//q_40 - gets a lower quality image to reduce load time
+//t_default - gets a lower quality image and cropped to 1000px
